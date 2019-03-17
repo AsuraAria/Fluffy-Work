720d13dd6988ced904fd46bab9401e88cda746f9
Elex <elex17@hotmail.fr>
2019-03-17 05:51:06 +0100

    zer

--- main.py
+++ main.py
@@ -29,15 +29,21 @@ frameSize = pygame.display.get_surface().get_size()
 fond = pygame.image.load("map/fond.png").convert()
 
 #Chargement de la map
-maps = pygame.image.load("map/map0.png").convert()
-maps = pygame.transform.scale(maps,(3000,3000))
-mapsPos = maps.get_rect()
-mapsPos = mapsPos.move(-290,-1560)
-
-mapsCollision = pygame.image.load("map/map0Mask.png").convert()
-mapsCollision = pygame.transform.scale(mapsCollision,(3000,3000))
+maps = []
+mapsPos = []
+mapsCollision = []
 pxarray = []
-pxarray = pygame.PixelArray(mapsCollision.copy()) #248 177 33
+
+for i in range(1):
+    maps.append(pygame.image.load("map/map0.png").convert())
+    maps[i] = pygame.transform.scale(maps[i],(3000,3000))
+    mapsPos.append(maps[i].get_rect())
+    mapsPos[i] = mapsPos[i].move(-290,-1560)
+    
+    mapsCollision.append(pygame.image.load("map/map0Mask.png").convert())
+    mapsCollision[i] = pygame.transform.scale(mapsCollision[i],(3000,3000))
+    
+    pxarray.append(pygame.PixelArray(mapsCollision[i].copy()))
 
 #Chargement et collage du personnage
 player = [[],[]]
@@ -49,7 +55,7 @@ for i in range(4):
     player[1][i] = pygame.transform.scale(player[1][i],(70,70))
 #frame.blit(perso, (200,300))
 
-botsPos = [[0,792.0, 1180, "?!#*&@"],[1,1794.0, 432.0, "?!#*&@"]]
+botsPos = [[0,792.0, 1180, "?!#*&@"],[1,1794.0, 432.0, "?!#*&@"],[3,2586.0, 250.0,"door"]]
 botsDiag = [["Fluffy : Hi, my name is Fluffy and I was hired",
              "in the archeologic team here. I am here to",
              "help with the new language you discovered.",
@@ -68,7 +74,7 @@ botsDiag = [["Fluffy : Hi, my name is Fluffy and I was hired",
              ""]]
             
 bots = []
-for i in range(2):
+for i in range(3):
     bots.append(pygame.image.load("bots/bot"+str(i)+".png").convert_alpha())
     bots[i] = pygame.transform.scale(bots[i],(70,70))
 
@@ -111,7 +117,7 @@ resetSpace = True;
 
 # Var Map
 
-mapPos = [0,0];
+currentMap = 0;
 
 # Var Player
 
@@ -137,29 +143,30 @@ constantiaFont = pygame.font.SysFont("constantia", 54)
 
 def isColliding(side):
     #global mapsCollision
+    global currentMap
     global mapsPos
     global pxarray
 
     wallDetected = False
 
-    upL = (int(W/2-mapsPos[0]),int(H/2-mapsPos[1]+40))
-    upR = (int(W/2-mapsPos[0]+70), int(H/2-mapsPos[1]+40))
-    doL = (int(W/2-mapsPos[0]), int(H/2-mapsPos[1]+70))
-    doR = (int(W/2-mapsPos[0]+70), int(H/2-mapsPos[1]+70))
+    upL = (int(W/2-mapsPos[currentMap][0]),int(H/2-mapsPos[currentMap][1]+40))
+    upR = (int(W/2-mapsPos[currentMap][0]+70), int(H/2-mapsPos[currentMap][1]+40))
+    doL = (int(W/2-mapsPos[currentMap][0]), int(H/2-mapsPos[currentMap][1]+70))
+    doR = (int(W/2-mapsPos[currentMap][0]+70), int(H/2-mapsPos[currentMap][1]+70))
 
     #print(pygame.Color(pxarray[doR[0]][doR[1]]))
 
     if (side == 0):
-        if (pygame.Color(pxarray[doL[0]-5][doL[1]]) == (0, 248, 177, 33) or pygame.Color(pxarray[upL[0]-5][upL[1]]) == (0, 248, 177, 33)):
+        if (pygame.Color(pxarray[currentMap][doL[0]-5][doL[1]]) == (0, 248, 177, 33) or pygame.Color(pxarray[currentMap][upL[0]-5][upL[1]]) == (0, 248, 177, 33)):
             return True
     if (side == 1):
-        if (pygame.Color(pxarray[upL[0]][upL[1]-5]) == (0, 248, 177, 33) or pygame.Color(pxarray[upR[0]][upR[1]-5]) == (0, 248, 177, 33)):
+        if (pygame.Color(pxarray[currentMap][upL[0]][upL[1]-5]) == (0, 248, 177, 33) or pygame.Color(pxarray[currentMap][upR[0]][upR[1]-5]) == (0, 248, 177, 33)):
             return True;
     if (side == 2):
-        if (pygame.Color(pxarray[upR[0]+5][upR[1]]) == (0, 248, 177, 33) or pygame.Color(pxarray[doR[0]+5][doR[1]]) == (0, 248, 177, 33)):
+        if (pygame.Color(pxarray[currentMap][upR[0]+5][upR[1]]) == (0, 248, 177, 33) or pygame.Color(pxarray[currentMap][doR[0]+5][doR[1]]) == (0, 248, 177, 33)):
             return True;
     if (side == 3):
-        if (pygame.Color(pxarray[doL[0]][doL[1]+5]) == (0, 248, 177, 33) or pygame.Color(pxarray[doR[0]][doR[1]+5]) == (0, 248, 177, 33)):
+        if (pygame.Color(pxarray[currentMap][doL[0]][doL[1]+5]) == (0, 248, 177, 33) or pygame.Color(pxarray[currentMap][doR[0]][doR[1]+5]) == (0, 248, 177, 33)):
             return True;
 
     return wallDetected
@@ -175,12 +182,18 @@ def isBot():
     botDetected = False
 
     for i in range(len(bots)):
-        if (sqrt((-mapsPos[0]+W/2-botsPos[i][1])**2+(-mapsPos[1]+H/2-botsPos[i][2])**2) <= 150):
+        if (sqrt((-mapsPos[currentMap][0]+W/2-botsPos[i][1])**2+(-mapsPos[currentMap][1]+H/2-botsPos[i][2])**2) <= 150):
             return botsPos[i][0]
     
     return -1
     #if (side == 0):
 
+def evaluateDoor():
+    global nearBot
+    
+    if (nearBot == 2):
+        currentMap = 1;
+        
 
 #=========================
 # While
@@ -243,33 +256,33 @@ while continuer:
         if (not isInteracting):
             if (direction[0]==1 or direction[1]==1 or direction[2]==1 or direction[3]==1):
                 sprite = (sprite+1)%spriteCount
-                if (direction[0] == 1 and -mapsPos[0]+W/2>=0 and not isColliding(0)):
+                if (direction[0] == 1 and -mapsPos[currentMap][0]+W/2>=0 and not isColliding(0)):
                     finalDir = 0
-                    mapsPos = mapsPos.move(-speed, 0);
+                    mapsPos[currentMap] = mapsPos[currentMap].move(-speed, 0);
                     keyTuto[0] = 1
-                if (direction[1] == 1 and -mapsPos[1]+H/2>=0 and not isColliding(1)):
+                if (direction[1] == 1 and -mapsPos[currentMap][1]+H/2>=0 and not isColliding(1)):
                     finalDir = 1
-                    mapsPos = mapsPos.move(0, -speed);
+                    mapsPos[currentMap] = mapsPos[currentMap].move(0, -speed);
                     keyTuto[1] = 1
-                if (direction[2] == 1 and -mapsPos[0]+W/2+70<=2990 and not isColliding(2)):
+                if (direction[2] == 1 and -mapsPos[currentMap][0]+W/2+70<=2990 and not isColliding(2)):
                     finalDir = 2
-                    mapsPos = mapsPos.move(speed, 0);
+                    mapsPos[currentMap] = mapsPos[currentMap].move(speed, 0);
                     keyTuto[2] = 1
-                if (direction[3] == 1 and -mapsPos[1]+H/2+70<=2990 and not isColliding(3)):
+                if (direction[3] == 1 and -mapsPos[currentMap][1]+H/2+70<=2990 and not isColliding(3)):
                     finalDir = 3
-                    mapsPos = mapsPos.move(0, speed);
+                    mapsPos[currentMap] = mapsPos[currentMap].move(0, speed);
                     keyTuto[3] = 1
             else:
                 sprite = 0
         
         #if (menu == 0):
         frame.blit(fond, (0,0))
-        frame.blit(maps, mapsPos)
-        #print((-mapsPos[0]+W/2,-mapsPos[1]+H/2))
+        frame.blit(maps[currentMap], mapsPos[currentMap])
+        print((-mapsPos[currentMap][0]+W/2,-mapsPos[currentMap][1]+H/2))
         
         #Bots
         for i in range(len(bots)):
-            frame.blit(bots[0],(botsPos[i][1]+mapsPos[0],botsPos[i][2]+mapsPos[1]))
+            frame.blit(bots[i],(botsPos[i][1]+mapsPos[currentMap][0],botsPos[i][2]+mapsPos[currentMap][1]))
             
         if (direction[1] == 1 and not isInteracting):
             frame.blit(player[int(sprite/(spriteCount/2))][1], (W/2,H/2))
@@ -291,21 +304,24 @@ while continuer:
         
         
         if (isInteracting):
-            print(nearBot)
-            frame.blit(box,(40,H-250-20))
-            if (whichDiag <= len(botsDiag[nearBot])-1):
-                text1 = constantiaFont.render(botsDiag[nearBot][whichDiag],1,White)
-                text2 = constantiaFont.render(botsDiag[nearBot][whichDiag+1],1,White)
-                text3 = constantiaFont.render(botsDiag[nearBot][whichDiag+2],1,White)
-                frame.blit(text1,(110,H-250+20))
-                frame.blit(text2,(110,H-250+80))
-                frame.blit(text3,(110,H-250+140))
+            #print(nearBot)
+            if (botsPos[nearBot][3] != "door"):
+                frame.blit(box,(40,H-250-20))
+                if (whichDiag <= len(botsDiag[nearBot])-1):
+                    text1 = constantiaFont.render(botsDiag[nearBot][whichDiag],1,White)
+                    text2 = constantiaFont.render(botsDiag[nearBot][whichDiag+1],1,White)
+                    text3 = constantiaFont.render(botsDiag[nearBot][whichDiag+2],1,White)
+                    frame.blit(text1,(110,H-250+20))
+                    frame.blit(text2,(110,H-250+80))
+                    frame.blit(text3,(110,H-250+140))
+                else:
+                    isInteracting = False;
+                    whichDiag = 0
+                    if (nearBot == 1):
+                       dicodecFound = True
+                sprite = 0
             else:
-                isInteracting = False;
-                whichDiag = 0
-                if (nearBot == 1):
-                   dicodecFound = True
-            sprite = 0
+                evaluteDoor()
         
     # UPDATE
     
